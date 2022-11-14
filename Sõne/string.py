92ea68788b89f5eee042a552ea1946f3c4a3b27b
Rasmus <rasmus.klaaser@voco.ee>
2022-11-14 15:40:35 +0200

    string/sõne

---
+++ Sõne/string.py
@@ -0,0 +1,18 @@
+first_name = "James"
+last_name = "Bond"
+
+full_name = first_name + " " + last_name
+#print(full_name)
+
+self_description_sentence = "My name is " + full_name
+#print(self_description_sentence)
+
+cake = "vahukoormarjadtäidispõhi"
+#print("vahu\nkoor\nmarjad\ntäidis\npõhi")
+
+
+original_string = "Programming is fun!"
+backwards= original_string[::-1]
+every_other = original_string[::2]
+first_word_reversed = original_string[0:11][::-1]
+print(first_word_reversed)
---
+++ function moodle.py
@@ -0,0 +1,29 @@
+"""Function examples."""
+
+
+# func()
+def func():
+    print("I´m inside the function")
+
+
+# my_name_is(name)
+def my_name_is(name):
+    print("My name is " + name)
+
+
+# sum_six(num)`
+def sum_xix(num):
+    return num + 6
+
+
+
+# sum_numbers()
+def sum_numbers(a, b):
+    return a + b
+
+
+# usd_to_eur()
+def usd_to_eur(int):
+    return 1 * 0.8
+
+print(usd_to_eur(80))
